4c71db28431c9960b4f278f2e38450273174c7c7
Pericyclic <marcel.ruth@icloud.com>
2023-07-27 00:29:03 +0200

    Added Publications

--- script.js
+++ script.js
@@ -40,8 +40,33 @@ let step = () => {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // Draw the graph
-    // ...
+    nodes.forEach(node => {
+        node.x += node.vx;
+        node.y += node.vy;
 
+        if (node.x < 0 || node.x > canvas.width) node.vx = -node.vx;
+        if (node.y < 0 || node.y > canvas.height) node.vy = -node.vy;
+
+        ctx.beginPath();
+        ctx.arc(node.x, node.y, 2, 0, 2 * Math.PI);
+        ctx.fillStyle = "#FFF";
+        ctx.fill();
+    });
+
+    edges.forEach(edge => {
+        let dx = edge.node1.x - edge.node2.x;
+        let dy = edge.node1.y - edge.node2.y;
+        let dist = Math.sqrt(dx * dx + dy * dy);
+
+        if (dist < 200) {
+            ctx.beginPath();
+            ctx.moveTo(edge.node1.x, edge.node1.y);
+            ctx.lineTo(edge.node2.x, edge.node2.y);
+            ctx.strokeStyle = `rgba(255, 255, 255, ${ 1 - dist / 200 })`;
+            ctx.stroke();
+        }
+    });
+    
     // Draw the molecules
     molecules.forEach(molecule => {
         molecule.x += molecule.vx;
